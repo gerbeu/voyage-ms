51b14a807c3b60139de96acb4d32727480ca2f20
Eugen Gerb <egb05@gmx.de>
2022-11-20 15:08:39 +0100

    extend instrumentation by adding span that contains produced/processed event

--- src/main/java/ibm/eda/kc/voyagems/infra/events/order/OrderAgent.java
+++ src/main/java/ibm/eda/kc/voyagems/infra/events/order/OrderAgent.java
@@ -1,5 +1,6 @@
 package ibm.eda.kc.voyagems.infra.events.order;
 
+import java.text.MessageFormat;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 import java.util.logging.Logger;
@@ -7,7 +8,12 @@ import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.smallrye.reactive.messaging.TracingMetadata;
 import org.eclipse.microprofile.reactive.messaging.*;
@@ -17,6 +23,8 @@ import ibm.eda.kc.voyagems.infra.events.voyage.VoyageAllocated;
 import ibm.eda.kc.voyagems.infra.events.voyage.VoyageEvent;
 import ibm.eda.kc.voyagems.infra.events.voyage.VoyageEventProducer;
 import ibm.eda.kc.voyagems.infra.repo.VoyageRepository;
+
+import static io.smallrye.reactive.messaging.kafka.KafkaConnector.TRACER;
 
 /**
  * Listen to the orders topic and processes event from order service:
@@ -37,20 +45,21 @@ public class OrderAgent {
     @Incoming("orders")
     public CompletionStage<Void> processOrder(Message<OrderEvent> messageWithOrderEvent) {
         logger.info("Received order : " + messageWithOrderEvent.getPayload().orderID);
-        OrderEvent oe = messageWithOrderEvent.getPayload();
+        OrderEvent orderEvent = messageWithOrderEvent.getPayload();
         Optional<TracingMetadata> optionalTracingMetadata = TracingMetadata.fromMessage(messageWithOrderEvent);
         if (optionalTracingMetadata.isPresent()) {
             TracingMetadata tracingMetadata = optionalTracingMetadata.get();
-            try (Scope scope = tracingMetadata.getCurrentContext().makeCurrent()) {
-                logger.info("TraceId " + Span.current().getSpanContext().getTraceId());
-                switch (oe.getType()) {
+            Context context = tracingMetadata.getCurrentContext();
+            try (Scope scope = context.makeCurrent()) {
+                createProcessedOrderEventSpan(orderEvent, context);
+                switch (orderEvent.getType()) {
                     case OrderEvent.ORDER_CREATED_TYPE:
-                        processOrderCreatedEvent(oe);
+                        processOrderCreatedEvent(orderEvent);
                         break;
                     case OrderEvent.ORDER_UPDATED_TYPE:
-                        logger.info("Receive order update " + oe.status);
-                        if (oe.status.equals(OrderEvent.ORDER_ON_HOLD_TYPE)) {
-                            compensateOrder(oe.orderID, oe.quantity);
+                        logger.info("Receive order update " + orderEvent.status);
+                        if (orderEvent.status.equals(OrderEvent.ORDER_ON_HOLD_TYPE)) {
+                            compensateOrder(orderEvent.orderID, orderEvent.quantity);
                         } else {
                             logger.info("Do future processing in case of order update");
                         }
@@ -62,6 +71,21 @@ public class OrderAgent {
             }
         }
         return messageWithOrderEvent.ack();
+    }
+
+    private void createProcessedOrderEventSpan(final OrderEvent orderEvent, final Context context) {
+        final String spanName = MessageFormat.format("processed event[{0}]", orderEvent.getType());
+        final SpanBuilder spanBuilder = TRACER.spanBuilder(spanName).setParent(context);
+        final Span span = spanBuilder.startSpan();
+        final ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            final String orderEventJson = ow.writeValueAsString(orderEvent);
+            span.setAttribute("processed.event", orderEventJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            span.end();
+        }
     }
 
     /**
--- src/main/java/ibm/eda/kc/voyagems/infra/events/voyage/VoyageEventProducer.java
+++ src/main/java/ibm/eda/kc/voyagems/infra/events/voyage/VoyageEventProducer.java
@@ -1,10 +1,17 @@
 package ibm.eda.kc.voyagems.infra.events.voyage;
 
+import java.text.MessageFormat;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import ibm.eda.kc.voyagems.infra.events.order.OrderEvent;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
 import io.opentelemetry.context.Context;
 import io.smallrye.reactive.messaging.TracingMetadata;
 import org.eclipse.microprofile.reactive.messaging.Channel;
@@ -12,6 +19,8 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
 
 import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
+
+import static io.smallrye.reactive.messaging.kafka.KafkaConnector.TRACER;
 
 @ApplicationScoped
 public class VoyageEventProducer {
@@ -23,6 +32,8 @@ public class VoyageEventProducer {
 
     public void sendEvent(String key, VoyageEvent voyageEvent) {
         logger.info("Send voyage message --> " + voyageEvent.voyageID + " ts: " + voyageEvent.getTimestampMillis());
+        Context context = Context.current();
+        createVoyageEventProducedSpan(voyageEvent, context);
         eventProducer.send(Message.of(voyageEvent)
                 .addMetadata(OutgoingKafkaRecordMetadata.<String>builder().withKey(key).build())
                 .withAck(() -> {
@@ -31,8 +42,23 @@ public class VoyageEventProducer {
                 .withNack(throwable -> {
                     return CompletableFuture.completedFuture(null);
                 })
-                .addMetadata(TracingMetadata.withCurrent(Context.current()))
+                .addMetadata(TracingMetadata.withCurrent(context))
         );
+    }
+
+    private void createVoyageEventProducedSpan(final VoyageEvent voyageEvent, final Context context) {
+        final String spanName = MessageFormat.format("produced event[{0}]", voyageEvent.getType());
+        final SpanBuilder spanBuilder = TRACER.spanBuilder(spanName).setParent(context);
+        final Span span = spanBuilder.startSpan();
+        final ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            final String voyageEventJson = ow.writeValueAsString(voyageEvent);
+            span.setAttribute("produced.event", voyageEventJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            span.end();
+        }
     }
 
 }
